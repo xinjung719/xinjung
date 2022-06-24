0d0ae95bf3467a4ce1efe5c80e99ea9e35d38631
JennyXJ <jenny860719jy2@gmail.com>
2022-06-24 19:43:36 +0800

    about
    about

--- js/footer.js
+++ js/footer.js
@@ -1,43 +1,43 @@
 document.getElementById("footer").innerHTML = `<footer class="bd-footer py-5 mt-5 bg-dark">
-            <div class="container ">
-                <div class="row">
-                    <div class="col text-center">
+            <div class="container text-center">
+                <div class="">
+                    <div class="col">
                         <a class="d-inline-flex">
-                            <img src="/xinjung/img/logo.png" height="50px">
+                            <img src="img/logo.png" height="50px">
                         </a>
                         <ul class="list-unstyled small text-muted">
                             <li class="mb-2 text-white">XinJung . 2022</li>
                         </ul>
                     </div>
                     <div class="col">
+                        <hr class="text-white">
                         <table class="table table table-borderless">
                             <tr>
                                 <td class="fw-bold">
-                                    <a href="/xinjung/index.html" class="text-decoration-none text-white">首頁</a>
+                                    <a href="/xinjung/index.html" class="text-decoration-none text-secondary">首頁</a>
                                 </td>
                                 <td class="fw-bold">
-                                    <a href="/xinjung/app.html" class="text-decoration-none text-white">APP設計</a>
+                                    <a href="/xinjung/app.html" class="text-decoration-none text-secondary">APP設計</a>
                                 </td>
                             </tr>
                             <tr>
                                 <td class="mb-2 fw-bold">
-                                    <a href="/xinjung/web.html" class="text-decoration-none text-white">Web製作</a>
+                                    <a href="/xinjung/web.html" class="text-decoration-none text-secondary">Web製作</a>
                                 </td>
                                 <td class="mb-2">
-                                    <a href="/xinjung/apppages/pmtree.html" class="text-decoration-none text-white">PM Tree</a></td>
+                                    <a href="/xinjung/apppages/pmtree.html" class="text-decoration-none text-secondary">PM Tree</a>
+                                </td>
                             </tr>
                             <tr>
-                                <td class="mb-2"><a href="https://thinkabit.ntue.edu.tw/" class="text-decoration-none text-white" target="_blank">微想實驗室</a></td>
-                                <td class="mb-2"><a href="/xinjung/apppages/trash.html" class="text-decoration-none text-white">Trash+</a></td>
+                                <td class="mb-2"><a href="https://thinkabit.ntue.edu.tw/" class="text-decoration-none text-secondary" target="_blank">微想實驗室</a></td>
+                                <td class="mb-2"><a href="/xinjung/apppages/trash.html" class="text-decoration-none text-secondary">Trash+</a></td>
                             </tr>
                             <tr>
-                                <td class="mb-2 fw-bold"><a href="/xinjung/about.html" class="text-decoration-none text-white">關於我</a></td>
-                                <td class="mb-2"><a href="/xinjung/apppages/argame.html" class="text-decoration-none text-white">搶救實驗室</a></td>
+                                <td class="mb-2 fw-bold"><a href="/xinjung/about.html" class="text-decoration-none text-secondary">關於我</a></td>
+                                <td class="mb-2"><a href="/xinjung/apppages/argame.html" class="text-decoration-none text-secondary">搶救實驗室</a></td>
                             </tr>
                         </table>
-
                     </div>
-
                 </div>
             </div>
         </footer>`;
