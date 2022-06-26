30c4a2fee7f9ee5d0942c45a75f4f0d49e3995a3
JennyXJ <jenny860719jy2@gmail.com>
2022-06-26 18:30:08 +0800

    TOP
    TOP

--- js/footer.js
+++ js/footer.js
@@ -40,4 +40,8 @@ document.getElementById("footer").innerHTML = `<footer class="bd-footer py-5 mt-
                     </div>
                 </div>
             </div>
-        </footer>`;
+        </footer>
+<a href="#" type="button" style="position:fixed;bottom:20px;right:20px;background-color: rgba(0, 0, 0, 0);border-color: rgba(0, 0, 0, 0)">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"  fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16" style="color: rgba(255, 255, 255, 0.5)"  ><path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+        </svg>
+    </a>`;
--- js/header.js
+++ js/header.js
@@ -6,7 +6,7 @@ document.getElementById("header").innerHTML = `<nav class="navbar navbar-dark" s
             <a class="navbar-brand" href="#">
                 <img src="/xinjung/img/logo.png" alt="欣君的網站" width="70">
             </a>
-            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" style="background-color: rgba(255, 255, 255, 0.8)">
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" style="background-color: rgba(8, 63, 74, 0.67)">
                 <div class="offcanvas-header" style="background-color: rgb(31, 72, 90)">
                     <h5 class="offcanvas-title" id="offcanvasExampleLabel">
                         <img src="/xinjung/img/logo.png" alt="欣君的網站" width="70">
@@ -16,10 +16,21 @@ document.getElementById("header").innerHTML = `<nav class="navbar navbar-dark" s
                 <div class="offcanvas-body ">
                     <ul class="navbar-nav justify-content-end flex-grow-1 ">
                         <div class="d-grid gap-2">
-                            <a class="btn btn-outline-secondary" href="/xinjung/index.html" role="button">首頁</a>
-                            <a class="btn btn-outline-secondary" href="/xinjung/app.html" role="button">APP 設計</a>
-                            <a class="btn btn-outline-secondary" href="/xinjung/app.html" role="button">Web製作</a>
-                            <a class="btn btn-outline-secondary" href="/xinjung/app.html" role="button">關於我</a>
+                            <a class="btn btn-outline-light" href="/xinjung/index.html" role="button">首頁</a>
+                                 <button class="btn btn-outline-light dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">APP 設計
+                                 </button>
+                                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                     <li><a class="dropdown-item" href="/xinjung/app.html">APP 設計</a>
+                                     </li>  <li><hr class="dropdown-divider"></li>
+                                     <li><a class="dropdown-item" href="/xinjung/apppages/argame.html">搶救實驗室</a>
+                                     </li>  <li><hr class="dropdown-divider"></li>
+                                     <li><a class="dropdown-item" href="/xinjung/apppages/pmtree.html">PM TREE</a>
+                                     </li>  <li><hr class="dropdown-divider"></li>
+                                     <li><a class="dropdown-item" href="/xinjung/apppages/trash.html">TRASH+</a>
+                                     </li>
+                                 </ul>
+                            <a class="btn btn-outline-light" href="/xinjung/web.html" role="button">Web製作</a>
+                            <a class="btn btn-outline-light" href="/xinjung/about.html" role="button">關於我</a>
                         </div>
                     </ul>
                 </div>
