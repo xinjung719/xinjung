9a06fd6776f104a6f488f268befa9522b23f4429
JennyXJ <jenny860719jy2@gmail.com>
2022-06-27 16:59:01 +0800

    100px
    100px

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ document.getElementById("footer").innerHTML = `<footer class="bd-footer py-5 mt-
                 <div class="">
                     <div class="col">
                         <a class="d-inline-flex">
-                            <img src="img/logo.png" height="50px">
+                            <img src="/xinjung/img/logo.png" height="50px">
                         </a>
                         <ul class="list-unstyled small text-muted">
                             <li class="mb-2 text-white">XinJung . 2022</li>
@@ -41,7 +41,7 @@ document.getElementById("footer").innerHTML = `<footer class="bd-footer py-5 mt-
                 </div>
             </div>
         </footer>
-<a href="#" type="button" style="position:fixed;bottom:20px;right:20px;background-color: rgba(0, 0, 0, 0);border-color: rgba(0, 0, 0, 0)">
+    <a href="#" type="button" style="position:fixed;bottom:20px;right:20px;background-color: rgba(0, 0, 0, 0);border-color: rgba(0, 0, 0, 0)">
         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"  fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16" style="color: rgba(255, 255, 255, 0.5)"  ><path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
         </svg>
     </a>`;
