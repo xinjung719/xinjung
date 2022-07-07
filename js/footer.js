8b8676b13ca1c60e13ea5de7d8f52a20168860e2
JennyXJ <jenny860719jy2@gmail.com>
2022-07-07 13:52:51 +0800

    fooder2
    fooder2

--- js/footer.js
+++ js/footer.js
@@ -1,9 +1,9 @@
 document.getElementById("footer").innerHTML = `<footer class="bd-footer py-5 mt-5 bg-dark">
             <div class="container text-center">
-                <div class="">
+                <div>
                     <div class="col">
                         <a class="d-inline-flex">
-                            <img src="https://raw.githubusercontent.com/xinjung719/xinjung/gh-pages/img/logo.png" height="50px">
+                            <img src="img/logo.png" height="50px">
                         </a>
                         <ul class="list-unstyled small text-muted">
                             <li class="mb-2 text-white">XinJung . 2022</li>
@@ -37,11 +37,15 @@ document.getElementById("footer").innerHTML = `<footer class="bd-footer py-5 mt-
                                 <td class="mb-2"><a href="/xinjung/apppages/argame.html" class="text-decoration-none text-secondary">搶救實驗室</a></td>
                             </tr>
                         </table>
+                    </div><br>
+                    <div class="col">
+                        <p class="text-white m-1">更多連結</p>
+                        <a class="btn btn-light" href="https://tw.linkedin.com/in/xinjung0719?trk=profile-badge" role="button"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                               <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                           </svg></a>
+                        <a class="btn text-white" href="https://www.cakeresume.com/me/jenny860719jy2" role="button" style="background-color: #1CA564">CakeResume</a>
+                        <a class="btn text-white" href="https://pda.104.com.tw/profile/share/bEvUTB78Zo6aw0cUtU3uzq8GR1FuWAnw" role="button" style="background-color: rgb(255, 123, 41);">104</a>
                     </div>
                 </div>
             </div>
-        </footer>
-    <a href="#" type="button" style="position:fixed;bottom:20px;right:20px;background-color: rgba(0, 0, 0, 0);border-color: rgba(0, 0, 0, 0)">
-        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"  fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16" style="color: rgba(255, 255, 255, 0.5)"  ><path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
-        </svg>
-    </a>`;
+        </footer>`;
